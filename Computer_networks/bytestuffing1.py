c8dd81dc9ab9e39cd23786626cd64cd6097cfc5d
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-07-01 22:44:10 +0530

    Update bytestuffing1.py

--- Computer_networks/bytestuffing1.py
+++ Computer_networks/bytestuffing1.py
@@ -1,10 +1,9 @@
-  
 a=input("enter data: ")
 flag=input("Enter startin delimiter ")
 esc=input("Enter ending delimiter ")
 print("the stuffed data: ",flag,end=" ")
 for i in a:
     if i in [esc,flag]:
-        print(i,end=" ")
+        print(esc,end=" ")
     print(i,end=" ")
 print(flag)
