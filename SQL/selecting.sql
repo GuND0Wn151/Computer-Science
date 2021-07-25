de498fe835e1e1e3ee2719afabf23cc528eda7fc
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-07-25 19:16:17 +0530

    selecting employee based on eno

---
+++ SQL/selecting.sql
@@ -0,0 +1,19 @@
+/* Employe table
+       ENO ENAME                       SAL
+---------- -------------------- ----------
+      1111 rest                        111
+         2 shyam                      1000
+         3 raghu                      5000
+         1 ram                        1111
+*/
+declare 
+empno emp.eno%type;
+ename emp.ename%type;
+sal emp.sal%type;
+e number:=&e;
+begin
+select eno,ename,sal into empno,ename,sal from emp where eno=e;
+dbms_output.put_line('Employee name is '||ename);
+dbms_output.put_line('Employee id and sal is '||empno||' '||sal);
+end;
+/
